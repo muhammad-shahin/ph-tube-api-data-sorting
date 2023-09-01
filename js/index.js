17b702b8f6444c90e7d2225f5a67d2750ce32c33
muhammad-shahin <muhammadshahin002@gmail.com>
2023-09-01 18:23:53 +0600

    responsive complete for tab & phone

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let currentCategoryId = '';
+let currentCategoryId = "";
 const loadCategory = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
@@ -6,28 +6,32 @@ const loadCategory = async () => {
   const data = await res.json();
   showCategoryButtons(data.data);
 };
+// showing category buttons in ui
 const showCategoryButtons = (categories) => {
   const categoryContainer = document.getElementById("category-container");
   categoryContainer.textContent = "";
-
   categories.forEach((category) => {
     const categoryBtn = document.createElement("button");
-    categoryBtn.classList = `bg-gray-200 text-xl px-5 py-2 rounded font-medium`;
     categoryBtn.id = category.category_id;
+    if(categoryBtn.id == 1000){
+      categoryBtn.classList = `active text-xl px-5 py-2 rounded font-medium`;
+    }else{
+      categoryBtn.classList = `not-active text-xl px-5 py-2 rounded font-medium`;
+    }
     categoryBtn.setAttribute(
       "onclick",
-      "loadVideos('" + category.category_id + "')"
+      "loadVideos('" + category.category_id + "'); changeButtonStyle(this)"
     );
     categoryBtn.innerText = category.category;
     categoryContainer.appendChild(categoryBtn);
   });
   loadVideos(categories[0].category_id);
 };
-// sort by views
-const sortByViewBtn = document.getElementById('sort-by-view-btn');
-sortByViewBtn.addEventListener('click', function(){
+// sort by views button click handler
+const sortByViewBtn = document.getElementById("sort-by-view-btn");
+sortByViewBtn.addEventListener("click", function () {
   loadVideos(currentCategoryId, true);
-})
+});
 const loadVideos = async (categoryId, isSorted = false) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
@@ -35,52 +39,13 @@ const loadVideos = async (categoryId, isSorted = false) => {
   const data = await res.json();
   const unsortedVideos = data.data;
   currentCategoryId = categoryId;
-  if (categoryId === "1000") {
-    document.getElementById(categoryId).style.backgroundColor = "#FF1F3D";
-    document.getElementById(categoryId).style.color = "#FFFFFF";
-    document.getElementById("1001").style.color = "#000000";
-    document.getElementById("1001").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1003").style.color = "#000000";
-    document.getElementById("1003").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1005").style.color = "#000000";
-    document.getElementById("1005").style.backgroundColor = "#E5E7EB";
-  } else if (categoryId === "1001") {
-    document.getElementById(categoryId).style.backgroundColor = "#FF1F3D";
-    document.getElementById(categoryId).style.color = "#FFFFFF";
-    document.getElementById("1000").style.color = "#000000";
-    document.getElementById("1000").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1003").style.color = "#000000";
-    document.getElementById("1003").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1005").style.color = "#000000";
-    document.getElementById("1005").style.backgroundColor = "#E5E7EB";
-  } else if (categoryId === "1003") {
-    document.getElementById(categoryId).style.backgroundColor = "#FF1F3D";
-    document.getElementById(categoryId).style.color = "#FFFFFF";
-    document.getElementById("1001").style.color = "#000000";
-    document.getElementById("1001").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1000").style.color = "#000000";
-    document.getElementById("1000").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1005").style.color = "#000000";
-    document.getElementById("1005").style.backgroundColor = "#E5E7EB";
-  } else if (categoryId === "1005") {
-    document.getElementById(categoryId).style.backgroundColor = "#FF1F3D";
-    document.getElementById(categoryId).style.color = "#FFFFFF";
-    document.getElementById("1001").style.color = "#000000";
-    document.getElementById("1001").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1003").style.color = "#000000";
-    document.getElementById("1003").style.backgroundColor = "#E5E7EB";
-    document.getElementById("1000").style.color = "#000000";
-    document.getElementById("1000").style.backgroundColor = "#E5E7EB";
-  }
-  
   if (data.status === true) {
-    if(isSorted === true){
+    if (isSorted === true) {
       const sortedVideos = unsortedVideos.sort(sortData);
       showVideos(sortedVideos);
-    }else{
+    } else {
       showVideos(unsortedVideos);
     }
-    
   } else if (data.status === false) {
     dataNotFound();
   }
@@ -89,7 +54,7 @@ const loadVideos = async (categoryId, isSorted = false) => {
 const showVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
   videoContainer.classList = [];
-  videoContainer.classList = `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6`;
+  videoContainer.classList = `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-12 mx-auto w-[85%] md:w-[85%] lg:w-auto`;
   videoContainer.textContent = "";
   videos.forEach((video) => {
     const thumbnail = video.thumbnail;
@@ -120,7 +85,6 @@ const showVideos = (videos) => {
           </div>
           <div class="flex justify-start items-center gap-3">
           <div class=""><img class="w-10 h-10 rounded-full" src="${authorProfilePic}"/></div>
-            
             <h4 class="font-bold">
             ${title}
             </h4>
@@ -149,17 +113,25 @@ const dataNotFound = () => {
   videoContainer.classList = `w-full h-[60vh] flex justify-center items-center flex-col gap-8`;
   videoContainer.innerHTML = `<img src="images/Icon.png" alt=""> <h1 class="text-4xl font-bold max-w-md text-center">Oops!! Sorry, There is no content here</h1>`;
 };
-
-
 const sortData = (data1, data2) => {
   const views1 = data1.others.views;
   const views2 = data2.others.views;
   const views1InNumber = views1.replace("K", "");
-  const viewNumber1 = parseFloat(views1InNumber);
   const views2InNumber = views2.replace("K", "");
+  const viewNumber1 = parseFloat(views1InNumber);
   const viewNumber2 = parseFloat(views2InNumber);
-  if(viewNumber1 < viewNumber2) return 1
-  else if(viewNumber1 > viewNumber2) return -1
+  if (viewNumber1 < viewNumber2) return 1;
+  else if (viewNumber1 > viewNumber2) return -1;
   return 0;
-}
+};
+const changeButtonStyle = (button) => {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((btn) => {
+    console.log(btn);
+    btn.classList.remove("active");
+    btn.classList.add("not-active");
+  });
+  button.classList.remove("not-active");
+  button.classList.add("active");
+};
 loadCategory();
